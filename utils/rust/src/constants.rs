27027b19968ee353430fc55f7bc56720d9a46ac6
Lazytangent <peter.tn.mai@gmail.com>
2022-12-01 17:01:02 -0600

    Update constants

--- utils/rust/src/constants.rs
+++ utils/rust/src/constants.rs
@@ -1,2 +1,2 @@
-pub const SAMPLE_DATA: &str = "data/small.txt";
-pub const REAL_DATA: &str = "data/full.txt";
+pub const SAMPLE_DATA: &str = "../data/small.txt";
+pub const REAL_DATA: &str = "../data/full.txt";
