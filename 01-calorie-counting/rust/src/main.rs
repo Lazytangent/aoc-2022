2b712151ffadc66271ef8a6bc2c062314d8a2313
Lazytangent <peter.tn.mai@gmail.com>
2022-12-01 14:22:45 -0600

    Working solution for day 01 problem 1 sample data

--- 01-calorie-counting/rust/src/main.rs
+++ 01-calorie-counting/rust/src/main.rs
@@ -1,3 +1,38 @@
+use std::{fs, io::Result, process};
+
+const SAMPLE_DATA: &str = "data/small.txt";
+const REAL_DATA: &str = "data/full.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = match read_file(SAMPLE_DATA) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error: {e:#?}");
+            process::exit(1);
+        }
+    };
+
+    let elves: Vec<String> = contents
+        .split("\n\n")
+        .map(String::from)
+        .collect();
+
+    let mut highest = 0;
+
+    for elf in elves {
+        let calories: usize = elf
+            .trim_end()
+            .split('\n')
+            .map(|s| str::parse(s).unwrap())
+            .reduce(|accum, item| accum + item)
+            .unwrap();
+
+        highest = highest.max(calories);
+    }
+
+    println!("First solution: {highest:#?}");
+}
+
+fn read_file(filename: &str) -> Result<String> {
+    fs::read_to_string(filename)
 }
