a4c25f0a845e1ee8c66645ede6563baa91739650
Lazytangent <peter.tn.mai@gmail.com>
2022-12-03 14:47:43 -0600

    Optimization for 03.2 with HashSet

--- 03-rucksack-reorganization/rust/src/lib.rs
+++ 03-rucksack-reorganization/rust/src/lib.rs
@@ -1,27 +1,29 @@
+use std::collections::HashSet;
+
 use utils::fs::DataType;
 
 pub fn solve(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
-    let rucksacks: Vec<String> = contents
-        .split('\n')
-        .map(String::from)
-        .collect();
+    let rucksacks: Vec<String> = contents.split('\n').map(String::from).collect();
 
     let mut sum = 0;
 
     for i in (0..rucksacks.len()).step_by(3) {
-        for c in rucksacks[i].chars() {
-            if rucksacks[i+1].contains(c) && rucksacks[i+2].contains(c) {
-                let priority = if c.is_lowercase() {
-                    (c as u8 - 'a' as u8 + 1) as usize
-                } else {
-                    (c as u8 - 'A' as u8) as usize + 27
-                };
+        let first: HashSet<char> = HashSet::from_iter(rucksacks[i].chars().collect::<Vec<char>>());
+        let second: HashSet<char> =
+            HashSet::from_iter(rucksacks[i + 1].chars().collect::<Vec<char>>());
+        let third: HashSet<char> =
+            HashSet::from_iter(rucksacks[i + 2].chars().collect::<Vec<char>>());
 
-                sum += priority;
-                break;
-            }
+        let first_second: HashSet<_> = first.intersection(&second).map(|c| c.to_owned()).collect();
+        let full = first_second.intersection(&third);
+        let symbol = full.last().unwrap();
+
+        if symbol.is_lowercase() {
+            sum += (*symbol as u8 - 'a' as u8 + 1) as usize;
+        } else {
+            sum += (*symbol as u8 - 'A' as u8) as usize + 27;
         }
     }
 
@@ -31,10 +33,7 @@ pub fn solve(r#type: DataType) {
 pub fn solve_one(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
-    let rucksacks: Vec<String> = contents
-        .split('\n')
-        .map(String::from)
-        .collect();
+    let rucksacks: Vec<String> = contents.split('\n').map(String::from).collect();
 
     let mut sum = 0;
 
