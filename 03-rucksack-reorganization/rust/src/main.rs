a26e8d564820a4b939545adc140bdec37c212edf
Lazytangent <peter.tn.mai@gmail.com>
2022-12-03 09:07:20 -0600

    Solved 03.2

--- 03-rucksack-reorganization/rust/src/main.rs
+++ 03-rucksack-reorganization/rust/src/main.rs
@@ -11,20 +11,20 @@ fn main() {
 
     let mut sum = 0;
 
-    for rucksack in rucksacks {
-        let n = rucksack.len() / 2;
-        for c in rucksack[..n].chars() {
-            if let Some(_) = rucksack[n..].find(c) {
+    for i in (0..rucksacks.len()).step_by(3) {
+        for c in rucksacks[i].chars() {
+            if rucksacks[i+1].contains(c) && rucksacks[i+2].contains(c) {
                 let priority = if c.is_lowercase() {
                     (c as u8 - 'a' as u8 + 1) as usize
                 } else {
                     (c as u8 - 'A' as u8) as usize + 27
                 };
+
                 sum += priority;
                 break;
             }
         }
     }
 
-    println!("First solution: {sum}");
+    println!("Second solution: {sum}");
 }
