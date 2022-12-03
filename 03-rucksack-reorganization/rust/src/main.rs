4a1fa28a7fbf2b49336d0753507fc883690a025f
Lazytangent <peter.tn.mai@gmail.com>
2022-12-03 10:42:12 -0600

    Update to use CLI util

--- 03-rucksack-reorganization/rust/src/main.rs
+++ 03-rucksack-reorganization/rust/src/main.rs
@@ -1,29 +1,9 @@
-use utils::{self, fs::DataType};
+use utils::{self, cli};
+
+use rucksack::solve;
 
 fn main() {
-    let contents = utils::fs::read_data(DataType::Real);
+    let cli = cli::run();
 
-    let rucksacks: Vec<String> = contents
-        .split('\n')
-        .map(String::from)
-        .collect();
-
-    let mut sum = 0;
-
-    for i in (0..rucksacks.len()).step_by(3) {
-        for c in rucksacks[i].chars() {
-            if rucksacks[i+1].contains(c) && rucksacks[i+2].contains(c) {
-                let priority = if c.is_lowercase() {
-                    (c as u8 - 'a' as u8 + 1) as usize
-                } else {
-                    (c as u8 - 'A' as u8) as usize + 27
-                };
-
-                sum += priority;
-                break;
-            }
-        }
-    }
-
-    println!("Second solution: {sum}");
+    solve(cli.r#type);
 }
