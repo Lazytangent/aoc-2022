deadbaae94817edc7e9b3cc938d7eaf850302df0
Lazytangent <peter.tn.mai@gmail.com>
2022-12-03 23:32:32 -0600

    Solved 04.2

--- 04-camp-cleanup/rust/src/lib.rs
+++ 04-camp-cleanup/rust/src/lib.rs
@@ -1,6 +1,6 @@
 use utils::{self, fs::DataType};
 
-pub fn solve(r#type: DataType) {
+pub fn solve_one(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
     let pairs: Vec<String> = contents.split('\n').map(String::from).collect();
@@ -21,6 +21,38 @@ pub fn solve(r#type: DataType) {
         if one[0] <= two[0] && one[1] >= two[1] {
             count += 1;
         } else if one[0] >= two[0] && one[1] <= two[1] {
+            count += 1;
+        }
+    }
+
+    println!("Part one solution: {count}");
+}
+
+pub fn solve(r#type: DataType) {
+    let contents = utils::fs::read_data(r#type);
+
+    let pairs: Vec<String> = contents.split('\n').map(String::from).collect();
+
+    let mut count = 0;
+
+    for pair in pairs {
+        let pair: Vec<&str> = pair.split(',').collect();
+        let one: Vec<u32> = pair[0]
+            .split('-')
+            .map(|s| str::parse::<u32>(s).unwrap())
+            .collect();
+        let two: Vec<u32> = pair[1]
+            .split('-')
+            .map(|s| str::parse::<u32>(s).unwrap())
+            .collect();
+
+        if one[0] <= two[0] && one[1] >= two[0] {
+            count += 1;
+        } else if one[0] >= two[0] && one[0] <= two[1] {
+            count += 1;
+        } else if one[1] >= two[0] && one[1] <= two[1] {
+            count += 1;
+        } else if two[1] >= one[0] && two[1] <= one[1] {
             count += 1;
         }
     }
