e607d5e1f1eca36e1da615ed85fa8a5e1225748c
Lazytangent <peter.tn.mai@gmail.com>
2022-12-04 23:13:19 -0600

    Basic splitting of data

--- 05/rust/src/lib.rs
+++ 05/rust/src/lib.rs
@@ -3,5 +3,10 @@ use utils::{self, fs::DataType};
 pub fn solve(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
-    let GROUPS: Vec<String> = contents.split('\n').map(String::from).collect();
+    let halves: Vec<String> = contents.split("\n\n").map(String::from).collect();
+    let drawing: Vec<String> = halves[0].split('\n').map(String::from).collect();
+    let moves: Vec<String> = halves[1].split('\n').map(String::from).collect();
+
+    println!("{drawing:#?}");
+    println!("{moves:#?}");
 }
