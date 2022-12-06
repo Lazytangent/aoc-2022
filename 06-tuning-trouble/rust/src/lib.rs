db04ee09f1d3c4e43fedc9da8d51053e370434a4
Lazytangent <peter.tn.mai@gmail.com>
2022-12-05 23:28:32 -0600

    Remove usage of .contains

--- 06-tuning-trouble/rust/src/lib.rs
+++ 06-tuning-trouble/rust/src/lib.rs
@@ -5,49 +5,39 @@ use utils::{self, fs::DataType};
 pub fn solve(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
-    let mut idx = 13;
+    let mut idx = 14;
 
     loop {
         if idx > contents.len() {
             break;
         }
 
-        let prev = &contents[(idx - 13)..idx];
+        let prev = &contents[(idx - 14)..idx];
 
         if prev.len() != HashSet::<char>::from_iter(prev.chars()).len() {
             idx += 1;
             continue;
         }
 
-        if prev.contains(contents.as_bytes()[idx] as char) {
-            idx += 1;
-            continue;
-        }
-
         break;
     }
 
-    println!("Part two solution: {}", idx + 1);
+    println!("Part two solution: {}", idx);
 }
 
 pub fn solve_one(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
-    let mut idx = 3;
+    let mut idx = 4;
 
     loop {
         if idx > contents.len() {
             break;
         }
 
-        let prev_three = &contents[(idx - 3)..idx];
+        let prev_three = &contents[(idx - 4)..idx];
 
         if prev_three.len() != HashSet::<char>::from_iter(prev_three.chars()).len() {
-            idx += 1;
-            continue;
-        }
-
-        if contents[(idx - 3)..idx].contains(contents.as_bytes()[idx] as char) {
             idx += 1;
             continue;
         }
@@ -55,5 +45,5 @@ pub fn solve_one(r#type: DataType) {
         break;
     }
 
-    println!("Part one solution: {}", idx + 1);
+    println!("Part one solution: {}", idx);
 }
