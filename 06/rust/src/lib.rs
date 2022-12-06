9f8d606dbd8573f184ca1b3d5898262fc9b79c5b
Lazytangent <peter.tn.mai@gmail.com>
2022-12-05 23:14:49 -0600

    Solved 06.2

--- 06/rust/src/lib.rs
+++ 06/rust/src/lib.rs
@@ -5,6 +5,34 @@ use utils::{self, fs::DataType};
 pub fn solve(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
+    let mut idx = 13;
+
+    loop {
+        if idx > contents.len() {
+            break;
+        }
+
+        let prev = &contents[(idx - 13)..idx];
+
+        if prev.len() != HashSet::<char>::from_iter(prev.chars()).len() {
+            idx += 1;
+            continue;
+        }
+
+        if prev.contains(contents.as_bytes()[idx] as char) {
+            idx += 1;
+            continue;
+        }
+
+        break;
+    }
+
+    println!("Part two solution: {}", idx + 1);
+}
+
+pub fn solve_one(r#type: DataType) {
+    let contents = utils::fs::read_data(r#type);
+
     let mut idx = 3;
 
     loop {
