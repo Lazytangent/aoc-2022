8d0714b57830cd7559207fd45e5a8bbb55310c80
Lazytangent <peter.tn.mai@gmail.com>
2022-12-07 09:13:14 -0600

    Part one solution

--- 07-no-space-left-on-device/python/main.py
+++ 07-no-space-left-on-device/python/main.py
@@ -1,36 +1,33 @@
-import enum
-from typing import Optional
-
-
-class NodeType(str, enum.Enum):
-    DIR = "dir"
-    FILE = "file"
-
-
-class TreeNode:
-    def __init__(self, name: str, type: NodeType, parent: Optional["TreeNode"] = None, size: int = 0):
-        self.name = name
-        self.type = type
-        self.parent = parent
-        self.size = size
-
-
-class FileTree:
-    def __init__(self):
-        self.root = None
-
-
 def parse():
-    file = "../data/small.txt"
-    # file = "../data/full.txt"
+    # file = "../data/small.txt"
+    file = "../data/full.txt"
 
     with open(file) as f:
         read_data = f.read()
 
     read_data = read_data.strip()
-    contents = read_data.split('\n')
+    contents = read_data.splitlines()
 
-    print(f"{contents}")
+    parent_dirs = []
+    dir_sizes = {}
+
+    for line in contents:
+        line = line.split(' ')
+
+        if line[0] == "$":
+            if line[1] == "cd" and line[2] != '..':
+                parent_dirs.append(line[2])
+                dir_sizes['/'.join(parent_dirs)] = 0
+            elif line[1] == "cd" and line[2] == "..":
+                parent_dirs.pop()
+        elif line[0].isnumeric():
+            temp_parent_dirs = parent_dirs.copy()
+            while len(temp_parent_dirs) > 0:
+                dir_sizes["/".join(temp_parent_dirs)] += int(line[0])
+                temp_parent_dirs.pop()
+
+    small_dirs = { k: v for (k, v) in dir_sizes.items() if v <= 100_000 }
+    print(f"Part one solution: {sum(small_dirs.values())}")
 
 
 if __name__ == "__main__":
