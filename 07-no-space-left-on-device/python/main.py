8a65dba9d020e3ffd70482e444b3ba716a804c94
Lazytangent <peter.tn.mai@gmail.com>
2022-12-07 09:21:16 -0600

    Solved 07.2"

--- 07-no-space-left-on-device/python/main.py
+++ 07-no-space-left-on-device/python/main.py
@@ -26,8 +26,29 @@ def parse():
                 dir_sizes["/".join(temp_parent_dirs)] += int(line[0])
                 temp_parent_dirs.pop()
 
+    total_disk_space = 70_000_000
+    required_free_space = 30_000_000
+
+    # print(f"Size of all directories: {dir_sizes}")
     small_dirs = { k: v for (k, v) in dir_sizes.items() if v <= 100_000 }
     print(f"Part one solution: {sum(small_dirs.values())}")
+
+    current_free_space = total_disk_space - dir_sizes['/']
+    # print(f"Current free space: {current_free_space}")
+
+    required_change = required_free_space - current_free_space
+    # print(f"Required change: {required_change}")
+
+    eligible_dirs = { k: v for (k, v) in dir_sizes.items() if v >= required_change }
+    # print(f"Eligible dirs: {eligible_dirs}")
+
+    smallest_eligible_dir = "/"
+
+    for (dir, size) in eligible_dirs.items():
+        if size < eligible_dirs[smallest_eligible_dir]:
+            smallest_eligible_dir = dir
+
+    print(f"Part two solution: {eligible_dirs[smallest_eligible_dir]}")
 
 
 if __name__ == "__main__":
