61407ba391af46054eb4b51a8d21e84c3493e38f
Lazytangent <peter.tn.mai@gmail.com>
2022-12-09 13:08:03 -0600

    Update cli util module to accept an arg for part

--- utils/rust/src/cli.rs
+++ utils/rust/src/cli.rs
@@ -7,6 +7,9 @@ pub struct Cli {
     /// Data file type to read
     #[arg(short = 't', long = "type", default_value_t = DataType::Sample)]
     pub r#type: DataType,
+    /// Part to run (1 or 2)
+    #[arg(short, long, default_value_t = 1)]
+    pub part: u8,
 }
 
 pub fn run() -> Cli {
