c87dbecb520f4b2b620571c048a8212a0f26dfc5
Lazytangent <peter.tn.mai@gmail.com>
2022-12-09 09:48:32 -0600

    Working on 09.2

--- 09-rope-bridge/rust/src/lib.rs
+++ 09-rope-bridge/rust/src/lib.rs
@@ -2,7 +2,101 @@ use std::collections::HashSet;
 
 use utils::{self, fs::DataType};
 
+pub fn solver(contents: String) {
+    let mut visited: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
+    let mut rope = vec![
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+    ];
+
+    let adjacent = [
+        (0, -1),
+        (0, 1),
+        (1, 0),
+        (-1, 0),
+        (1, -1),
+        (-1, -1),
+        (1, 1),
+        (-1, 1),
+        (0, 0),
+    ];
+
+    let mut insertions = vec![];
+
+    let instructions: Vec<String> = contents.split('\n').map(String::from).collect();
+
+    for line in instructions {
+        let line: Vec<&str> = line.split(' ').collect();
+        let direction = line[0];
+        let amount = str::parse::<u32>(line[1]).unwrap();
+
+        for _ in 0..amount {
+            let mut prev = rope[0];
+
+            match direction {
+                "U" => {
+                    rope[0] = (rope[0].0, rope[0].1 + 1);
+                }
+                "D" => {
+                    rope[0] = (rope[0].0, rope[0].1 - 1);
+                }
+                "L" => {
+                    rope[0] = (rope[0].0 - 1, rope[0].1);
+                }
+                "R" => {
+                    rope[0] = (rope[0].0 + 1, rope[0].1);
+                }
+                _ => ()
+            };
+
+            for idx in 1..rope.len() {
+                let mut move_node = true;
+                let node = rope[idx];
+
+                for dir in adjacent {
+                    let location = (node.0 + dir.0, node.1 + dir.1);
+
+                    if location == rope[idx - 1] {
+                        move_node = false;
+                        break;
+                    }
+                }
+
+                if move_node {
+                    rope[idx] = prev;
+
+                    prev = node;
+                    if idx == 9 {
+                        visited.insert(rope[idx]);
+                        insertions.push(rope[idx]);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("Insertions in order: {insertions:#?}");
+
+    println!("Part two solution: {}", visited.len());
+}
+
 pub fn solve(r#type: DataType) {
+    let contents = utils::fs::read_data(r#type);
+
+    solver(contents);
+}
+
+pub fn solve_one(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
 
     let mut visited: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
@@ -65,4 +159,26 @@ pub fn solve(r#type: DataType) {
     }
 
     println!("Part one solution: {}", visited.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solver;
+
+    const SAMPLE_TWO: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+";
+
+    #[test]
+    fn test_sample_input_two() {
+        let contents = SAMPLE_TWO.to_string();
+
+        solver(contents);
+    }
 }
