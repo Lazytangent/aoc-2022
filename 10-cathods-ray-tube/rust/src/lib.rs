dad03bbb2cf1a1f09783edf7da4bcdd703a8ad7a
Lazytangent <peter.tn.mai@gmail.com>
2022-12-10 10:17:07 -0600

    Solved 10.2

--- 10-cathods-ray-tube/rust/src/lib.rs
+++ 10-cathods-ray-tube/rust/src/lib.rs
@@ -4,22 +4,24 @@ pub fn solve(r#type: DataType, part: u8) {
     let contents = utils::fs::read_data(r#type);
 
     match part {
-        1 => part_one(contents),
+        1 => {
+            part_one(contents);
+        }
         2 => part_two(contents),
         _ => unreachable!(),
     };
 }
 
-pub fn part_one(contents: String) {
+pub fn part_one(contents: String) -> Vec<i32> {
     let instructions: Vec<String> = contents.split('\n').map(String::from).collect();
-    let mut reg_values: Vec<i32> = vec![1; 222];
+    let mut reg_values: Vec<i32> = vec![1; 241];
     let mut idx: usize = 2;
 
     for line in instructions {
         reg_values[idx] = reg_values[idx - 1];
         idx += 1;
 
-        if idx > 221 {
+        if idx > 240 {
             break;
         }
 
@@ -33,7 +35,7 @@ pub fn part_one(contents: String) {
         reg_values[idx] = reg_values[idx - 1] + amount;
         idx += 1;
 
-        if idx > 221 {
+        if idx > 240 {
             break;
         }
     }
@@ -46,6 +48,43 @@ pub fn part_one(contents: String) {
     }
 
     println!("Part one solution: {sum}");
+
+    reg_values
 }
 
-pub fn part_two(contents: String) {}
+pub fn part_two(contents: String) {
+    let mut screen = vec![vec!["."; 40]; 6];
+
+    let values = part_one(contents);
+    let mut row = 0;
+    let mut col = 0;
+
+    for cycle in 1..values.len() {
+        let val = values[cycle];
+
+        if cycle > 1 && cycle % 40 == 1 {
+            col = 0;
+            row += 1;
+        }
+
+        if col == val || col == val - 1 || col == val + 1 {
+            screen[row][col as usize] = "#";
+        }
+
+        col += 1;
+    }
+
+    print_screen(&screen);
+}
+
+fn print_screen(screen: &Vec<Vec<&str>>) {
+    let mut contents = String::new();
+
+    for row in screen {
+        let row: String = row.iter().map(|s| s.to_string()).collect();
+        contents.push_str(&row);
+        contents.push('\n');
+    }
+
+    println!("{contents}");
+}
