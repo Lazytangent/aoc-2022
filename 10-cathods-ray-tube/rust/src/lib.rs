ffafe6ae639847b960a5f1812a074d28e59e7863
Lazytangent <peter.tn.mai@gmail.com>
2022-12-10 09:21:41 -0600

    Solved 10.1

--- 10-cathods-ray-tube/rust/src/lib.rs
+++ 10-cathods-ray-tube/rust/src/lib.rs
@@ -11,7 +11,41 @@ pub fn solve(r#type: DataType, part: u8) {
 }
 
 pub fn part_one(contents: String) {
-    let GROUPS: Vec<String> = contents.split('\n').map(String::from).collect();
+    let instructions: Vec<String> = contents.split('\n').map(String::from).collect();
+    let mut reg_values: Vec<i32> = vec![1; 222];
+    let mut idx: usize = 2;
+
+    for line in instructions {
+        reg_values[idx] = reg_values[idx - 1];
+        idx += 1;
+
+        if idx > 221 {
+            break;
+        }
+
+        if line.starts_with("noop") {
+            continue;
+        }
+
+        let line = line.split(' ').collect::<Vec<&str>>();
+        let amount: i32 = line[1].parse().unwrap();
+
+        reg_values[idx] = reg_values[idx - 1] + amount;
+        idx += 1;
+
+        if idx > 221 {
+            break;
+        }
+    }
+
+    let interesting_indicies = [20, 60, 100, 140, 180, 220];
+    let mut sum = 0;
+
+    for i in interesting_indicies {
+        sum += reg_values[i] * i as i32;
+    }
+
+    println!("Part one solution: {sum}");
 }
 
 pub fn part_two(contents: String) {}
