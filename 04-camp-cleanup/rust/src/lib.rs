4e25cdf1b41893379e988c8e92b50f4562790a78
Lazytangent <peter.tn.mai@gmail.com>
2022-12-12 15:00:57 -0600

    Add tests for day 04

--- 04-camp-cleanup/rust/src/lib.rs
+++ 04-camp-cleanup/rust/src/lib.rs
@@ -2,7 +2,12 @@ use utils::{self, fs::DataType};
 
 pub fn solve_one(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
+    let count = part_one(&contents);
 
+    println!("Part one solution: {count}");
+}
+
+pub fn part_one(contents: &str) -> i32 {
     let pairs: Vec<String> = contents.split('\n').map(String::from).collect();
 
     let mut count = 0;
@@ -25,12 +30,17 @@ pub fn solve_one(r#type: DataType) {
         }
     }
 
-    println!("Part one solution: {count}");
+    count
 }
 
 pub fn solve(r#type: DataType) {
     let contents = utils::fs::read_data(r#type);
+    let count = part_two(&contents);
 
+    println!("Part two solution: {count}");
+}
+
+pub fn part_two(contents: &str) -> i32 {
     let pairs: Vec<String> = contents.split('\n').map(String::from).collect();
 
     let mut count = 0;
@@ -57,5 +67,33 @@ pub fn solve(r#type: DataType) {
         }
     }
 
-    println!("Part two solution: {count}");
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part_one, part_two};
+
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    #[test]
+    fn part_one_works() {
+        let expected = 2;
+        let actual = part_one(INPUT);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn part_two_works() {
+        let expected = 4;
+        let actual = part_two(INPUT);
+
+        assert_eq!(actual, expected);
+    }
 }
