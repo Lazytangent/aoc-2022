4e1461751878df8d408c7c60703e28ef0328a317
Lazytangent <peter.tn.mai@gmail.com>
2022-12-12 09:45:16 -0600

    Finish 12.1 in Rust

--- 12-hill-climbing-algorithm/rust/src/lib.rs
+++ 12-hill-climbing-algorithm/rust/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use utils::{self, fs::DataType};
 
 pub fn solve(r#type: DataType, part: u8) {
@@ -11,7 +13,82 @@ pub fn solve(r#type: DataType, part: u8) {
 }
 
 pub fn part_one(contents: String) {
-    let GROUPS: Vec<String> = contents.split('\n').map(String::from).collect();
+    let map: Vec<Vec<char>> = contents.split('\n').map(|s| s.chars().collect()).collect();
+    let height: usize = map.len();
+    let width: usize = map[0].len();
+
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+
+    let mut grid: Vec<Vec<usize>> = vec![vec![0; width]; height];
+
+    for i in 0..height {
+        for j in 0..width {
+            if map[i][j] == 'S' {
+                start = (i, j);
+            } else if map[i][j] == 'E' {
+                end = (i, j);
+                grid[i][j] = 26;
+            } else {
+                grid[i][j] = map[i][j] as usize - 'a' as usize;
+            }
+        }
+    }
+
+    let mut distances: Vec<Vec<usize>> = vec![vec![0; width]; height];
+    distances[start.0][start.1] = 0;
+    let mut seen: VecDeque<(usize, usize)> = VecDeque::new();
+    seen.push_back(start);
+
+    while let Some(point) = seen.pop_front() {
+        let steps = distances[point.0][point.1];
+
+        if point.0 + 1 < height {
+            let new = (point.0 + 1, point.1);
+            if (distances[new.0][new.1] == 0 && new != start) || steps + 1 < distances[new.0][new.1] {
+                if grid[new.0][new.1] <= grid[point.0][point.1] + 1 {
+                    distances[new.0][new.1] = steps + 1;
+                    seen.push_back(new);
+                }
+            }
+        }
+        if point.0 > 0 {
+            let new = (point.0 - 1, point.1);
+            if (distances[new.0][new.1] == 0 && new != start) || steps + 1 < distances[new.0][new.1] {
+                if grid[new.0][new.1] <= grid[point.0][point.1] + 1 {
+                    distances[new.0][new.1] = steps + 1;
+                    seen.push_back(new);
+                }
+            }
+        }
+        if point.1 + 1 < width {
+            let new = (point.0, point.1 + 1);
+            if (distances[new.0][new.1] == 0 && new != start) || steps + 1 < distances[new.0][new.1] {
+                if grid[new.0][new.1] <= grid[point.0][point.1] + 1 {
+                    distances[new.0][new.1] = steps + 1;
+                    seen.push_back(new);
+                }
+            }
+        }
+        if point.1 > 0 {
+            let new = (point.0, point.1 - 1);
+            if (distances[new.0][new.1] == 0 && new != start) || steps + 1 < distances[new.0][new.1] {
+                if grid[new.0][new.1] <= grid[point.0][point.1] + 1 {
+                    distances[new.0][new.1] = steps + 1;
+                    seen.push_back(new);
+                }
+            }
+        }
+    }
+
+    println!("{:#?}", distances[end.0][end.1]);
 }
+
+const OFFSETS: [(i32, i32); 4] = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
+];
 
 pub fn part_two(contents: String) {}
