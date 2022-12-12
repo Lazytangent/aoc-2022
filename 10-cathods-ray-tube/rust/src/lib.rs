048a16e5d82ca9461a3a8720abeb690d797ca0b9
Lazytangent <peter.tn.mai@gmail.com>
2022-12-12 13:31:46 -0600

    Add tests for day 10

--- 10-cathods-ray-tube/rust/src/lib.rs
+++ 10-cathods-ray-tube/rust/src/lib.rs
@@ -5,15 +5,34 @@ pub fn solve(r#type: DataType, part: u8) {
 
     match part {
         1 => {
-            part_one(contents);
+            let val = part_one(&contents);
+            println!("Part one solution: {val}");
         }
-        2 => part_two(contents),
+        2 => {
+            let screen = part_two(&contents);
+            let screen = print_screen(&screen);
+            println!("Part two solution:");
+            println!("{screen}");
+        }
         _ => unreachable!(),
     };
 }
 
-pub fn part_one(contents: String) -> Vec<i32> {
+pub fn part_one(contents: &str) -> i32 {
+    let reg_values = calculate_values(contents);
+    let interesting_indicies = [20, 60, 100, 140, 180, 220];
+    let mut sum = 0;
+
+    for i in interesting_indicies {
+        sum += reg_values[i] * i as i32;
+    }
+
+    sum
+}
+
+fn calculate_values(contents: &str) -> Vec<i32> {
     let instructions: Vec<String> = contents.split('\n').map(String::from).collect();
+
     let mut reg_values: Vec<i32> = vec![1; 241];
     let mut idx: usize = 2;
 
@@ -40,22 +59,13 @@ pub fn part_one(contents: String) -> Vec<i32> {
         }
     }
 
-    let interesting_indicies = [20, 60, 100, 140, 180, 220];
-    let mut sum = 0;
-
-    for i in interesting_indicies {
-        sum += reg_values[i] * i as i32;
-    }
-
-    println!("Part one solution: {sum}");
-
     reg_values
 }
 
-pub fn part_two(contents: String) {
+pub fn part_two(contents: &str) -> Vec<Vec<&str>> {
     let mut screen = vec![vec!["."; 40]; 6];
 
-    let values = part_one(contents);
+    let values = calculate_values(contents);
     let mut row = 0;
     let mut col = 0;
 
@@ -74,11 +84,10 @@ pub fn part_two(contents: String) {
         col += 1;
     }
 
-    println!("Part two solution:");
-    print_screen(&screen);
+    screen
 }
 
-fn print_screen(screen: &Vec<Vec<&str>>) {
+pub fn print_screen(screen: &Vec<Vec<&str>>) -> String {
     let mut contents = String::new();
 
     for row in screen {
@@ -87,5 +96,181 @@ fn print_screen(screen: &Vec<Vec<&str>>) {
         contents.push('\n');
     }
 
-    println!("{contents}");
+    contents
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part_one, part_two, print_screen};
+
+    const INPUT: &str = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+
+    #[test]
+    fn part_one_works() {
+        let expected = 13140;
+        let actual = part_one(INPUT);
+
+        assert_eq!(actual, expected);
+    }
+
+    const EXPECTED: &str = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+";
+
+    #[test]
+    fn part_two_works() {
+        let grid = part_two(INPUT);
+        let actual = print_screen(&grid);
+
+        assert_eq!(&actual, EXPECTED);
+    }
 }
