30bbd92b85309a57c477b28a65d58c6871c8cdea
Lazytangent <peter.tn.mai@gmail.com>
2022-12-14 11:54:46 -0600

    Update cli util to parse for a '-r' flag

--- utils/rust/src/cli.rs
+++ utils/rust/src/cli.rs
@@ -10,8 +10,21 @@ pub struct Cli {
     /// Part to run (1 or 2)
     #[arg(short, long, default_value_t = 1)]
     pub part: u8,
+    /// Whether or not to use the real data
+    #[arg(short, long, default_value_t = false)]
+    pub real: bool,
 }
 
 pub fn run() -> Cli {
     Cli::parse()
+}
+
+pub fn get_type(cli: &Cli) -> DataType {
+    match (cli.r#type, cli.real) {
+        (DataType::Sample, false) => DataType::Sample,
+        (DataType::Real, false) => DataType::Real,
+        (DataType::Sample, true) => DataType::Real,
+        (DataType::Real, true) => DataType::Real,
+        (DataType::SampleTwo, _) => DataType::SampleTwo,
+    }
 }
