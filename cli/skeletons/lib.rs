93f671c3cca8e83e47f11da435f74a5577e02002
Lazytangent <peter.tn.mai@gmail.com>
2022-12-14 10:31:54 -0600

    Update types in skeleton

--- cli/skeletons/lib.rs
+++ cli/skeletons/lib.rs
@@ -4,12 +4,12 @@ pub fn solve(r#type: DataType, part: u8) {
     let contents = utils::fs::read_data(r#type);
 
     let val = match part {
-        1 => part_one(contents),
-        2 => part_two(contents),
+        1 => part_one(&contents),
+        2 => part_two(&contents),
         _ => unreachable!(),
     };
 
-    println!("Part {part} solution: {val}");
+    println!("Part {part} solution: {val:?}");
 }
 
 pub fn part_one(contents: &str) {
