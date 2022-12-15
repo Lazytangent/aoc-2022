328b00fe35f129d30a9465841b68e868eae341e5
Lazytangent <peter.tn.mai@gmail.com>
2022-12-15 13:06:38 -0600

    Update cli tool

--- cli/src/skeleton/generate.rs
+++ cli/src/skeleton/generate.rs
@@ -4,12 +4,15 @@ use std::fs;
 pub struct Args {
     /// Name of the new package
     name: String,
+    /// Boolean to denote that the year is not 2022 (the 2022 version contains the utils lib)
+    #[arg(short, long, default_value_t = false)]
+    not: bool,
 }
 
 pub async fn run(args: Args) {
     let client = reqwest::Client::new();
 
-    let toml_file = get_toml_file(&client, &args.name).await;
+    let toml_file = get_toml_file(&client, &args.name, args.not).await;
     let main_file = get_main_file(&client, &args.name).await;
     let lib_file = get_lib_file(&client).await;
 
@@ -41,8 +44,13 @@ async fn get_lib_file(client: &reqwest::Client) -> String {
     get_file(client, "lib.rs").await
 }
 
-async fn get_toml_file(client: &reqwest::Client, name: &str) -> String {
-    let file = get_file(client, "Cargo.toml").await;
+async fn get_toml_file(client: &reqwest::Client, name: &str, not_2022: bool) -> String {
+    let filename = if not_2022 {
+        "Cargo_two.toml"
+    } else {
+        "Cargo.toml"
+    };
+    let file = get_file(client, filename).await;
 
     file.replace("PACKAGE", name)
 }
