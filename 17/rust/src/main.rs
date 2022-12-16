9b48dbb782c9a0a05ae97bade38310402b239fa8
Lazytangent <peter.tn.mai@gmail.com>
2022-12-16 09:19:01 -0600

    Set up day 17

---
+++ 17/rust/src/main.rs
@@ -0,0 +1,11 @@
+use utils;
+
+use seventeenth::solve;
+
+fn main() {
+    let cli = utils::cli::run();
+
+    let r#type = utils::cli::get_type(&cli);
+    let part = utils::cli::get_part(&cli);
+    solve(r#type, part);
+}
