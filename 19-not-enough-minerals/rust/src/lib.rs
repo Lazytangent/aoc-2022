86e297d0be6da03dd6dfabe4479169828c672df3
Lazytangent <peter.tn.mai@gmail.com>
2022-12-19 08:31:06 -0600

    Set up day 19

---
+++ 19-not-enough-minerals/rust/src/lib.rs
@@ -0,0 +1,18 @@
+use utils::{self, cli::Part, fs::DataType};
+
+pub fn solve(r#type: DataType, part: Part) {
+    let contents = utils::fs::read_data(r#type);
+
+    let val = match part {
+        Part::One => part_one(&contents),
+        Part::Two => part_two(&contents),
+    };
+
+    println!("Part {part} solution: {val:?}");
+}
+
+pub fn part_one(contents: &str) {
+    let GROUPS: Vec<&str> = contents.split('\n').collect();
+}
+
+pub fn part_two(contents: &str) {}
