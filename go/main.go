62bbbaff31cf495bd0fd85c9b22a291979491feb
Lazytangent <peter.tn.mai@gmail.com>
2023-04-15 20:34:47 -0500

    Create Go project skeleton

---
+++ go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"lazytangent/aoc/2022/utils"
+)
+
+func main() {
+	input, err := utils.ParseInput(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
