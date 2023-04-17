4bbcc8d01c550ebd3f2737f530aecd3c621d9883
Lazytangent <peter.tn.mai@gmail.com>
2023-04-16 20:45:02 -0500

    Go: 2.2

--- 02-rock-paper-scissors/go/main.go
+++ 02-rock-paper-scissors/go/main.go
@@ -17,44 +17,58 @@ func main() {
 	input = strings.TrimSpace(input)
 
 	score := 0
+	partTwo := 0
 	win, tie, loss := 6, 3, 0
 	rock, paper, scissors := 1, 2, 3
 	for _, round := range strings.Split(input, "\n") {
 		shapes := strings.Split(round, " ")
 
 		switch shapes[1] {
-		case "X": // Rock
+		case "X": // Rock // Loss
 			switch shapes[0] {
 			case "A": // Rock
 				score += tie
+				partTwo += scissors
 			case "B": // Paper
 				score += loss
+				partTwo += rock
 			case "C": // Scissors
 				score += win
+				partTwo += paper
 			}
 			score += rock
-		case "Y": // Paper
+			partTwo += loss
+		case "Y": // Paper // Tie
 			switch shapes[0] {
 			case "A": // Rock
 				score += win
+				partTwo += rock
 			case "B": // Paper
 				score += tie
+				partTwo += paper
 			case "C": // Scissors
 				score += loss
+				partTwo += scissors
 			}
 			score += paper
-		case "Z": // Scissors
+			partTwo += tie
+		case "Z": // Scissors // Win
 			switch shapes[0] {
 			case "A": // Rock
 				score += loss
+				partTwo += paper
 			case "B": // Paper
 				score += win
+				partTwo += scissors
 			case "C": // Scissors
 				score += tie
+				partTwo += rock
 			}
 			score += scissors
+			partTwo += win
 		}
 	}
 
 	fmt.Println("Part one:", score)
+	fmt.Println("Part two:", partTwo)
 }
