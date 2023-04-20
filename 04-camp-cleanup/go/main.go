01255149aa1993a782aa3ec59d0cddc4aae35575
Lazytangent <peter.tn.mai@gmail.com>
2023-04-20 11:42:09 -0500

    4.2 in Go

--- 04-camp-cleanup/go/main.go
+++ 04-camp-cleanup/go/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"lazytangent/aoc/2022/utils"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-
-	"lazytangent/aoc/2022/utils"
 )
 
 func main() {
@@ -19,30 +18,50 @@ func main() {
 	elves := strings.Split(input, "\n")
 
 	count := 0
+	partTwo := 0
 	for _, elf := range elves {
 		split := strings.Split(elf, ",")
 		left, right := split[0], split[1]
 
-		if (leftContainsRight(left, right) || rightContainsLeft(left, right)) {
+		if leftContainsRight(left, right) || rightContainsLeft(left, right) {
 			count++
+		}
+
+		if anyOverlap(left, right) {
+			partTwo++
 		}
 	}
 
 	fmt.Println("Part One:", count)
+	fmt.Println("Part Two:", partTwo)
 }
 
 func leftContainsRight(left, right string) bool {
-	leftRange := strings.Split(left, "-")
-	rightRange := strings.Split(right, "-")
-	leftStart, _ := strconv.Atoi(leftRange[0])
-	leftEnd, _ := strconv.Atoi(leftRange[1])
-	rightStart, _ := strconv.Atoi(rightRange[0])
-	rightEnd, _ := strconv.Atoi(rightRange[1])
+	leftRange, rightRange := getRanges(left, right)
 
-	return leftStart <= rightStart && leftEnd >= rightEnd
+	return leftRange.Start <= rightRange.Start && leftRange.End >= rightRange.End
 }
 
 func rightContainsLeft(left, right string) bool {
+	leftRange, rightRange := getRanges(left, right)
+
+	return rightRange.Start <= leftRange.Start && rightRange.End >= leftRange.End
+}
+
+func anyOverlap(left, right string) bool {
+	leftRange, rightRange := getRanges(left, right)
+
+	return (leftRange.Start >= rightRange.Start && leftRange.Start <= rightRange.End) ||
+		(leftRange.End >= rightRange.Start && leftRange.End <= rightRange.End) ||
+		(rightRange.Start >= leftRange.Start && rightRange.Start <= leftRange.End) ||
+		(rightRange.End >= leftRange.Start && rightRange.End <= leftRange.End)
+}
+
+type numRange struct {
+	Start, End int
+}
+
+func getRanges(left, right string) (numRange, numRange) {
 	leftRange := strings.Split(left, "-")
 	rightRange := strings.Split(right, "-")
 	leftStart, _ := strconv.Atoi(leftRange[0])
@@ -50,5 +69,5 @@ func rightContainsLeft(left, right string) bool {
 	rightStart, _ := strconv.Atoi(rightRange[0])
 	rightEnd, _ := strconv.Atoi(rightRange[1])
 
-	return rightStart <= leftStart && rightEnd >= leftEnd
+	return numRange{Start: leftStart, End: leftEnd}, numRange{Start: rightStart, End: rightEnd}
 }
