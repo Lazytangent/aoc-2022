a33a5c01909e7dcdcea3553659e84b1589895b85
Lazytangent <peter.tn.mai@gmail.com>
2023-04-21 12:02:26 -0500

    6.1 in C

---
+++ 06-tuning-trouble/c/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PART_ONE_LENGTH 4
+#define ALPHABET 26
+
+char *small_txt = "../data/small.txt";
+char *large_txt = "../data/full.txt";
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char *filename;
+
+    if (argc > 1 && strcmp(argv[1], "full") == 0) {
+        filename = large_txt;
+    } else {
+        filename = small_txt;
+    }
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error! Could not read data file: %s\n", filename);
+        exit(-1);
+    }
+
+    char *buffer = 0;
+    long length;
+    fseek(fp, 0, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    buffer = malloc(length);
+    if (buffer) {
+        fread(buffer, 1, length, fp);
+    }
+    fclose(fp);
+
+    int i;
+    for (i = PART_ONE_LENGTH - 1; i < length; i++) {
+        char contents[ALPHABET];
+        for (int j = 0; j < ALPHABET; j++) {
+            contents[j] = 0;
+        }
+
+        for (int j = i; j >= i - 3; j--) {
+            contents[buffer[j] - 'a'] = 1;
+        }
+
+        int sum = 0;
+        for (int j = 0; j < ALPHABET; j++) {
+            sum += contents[j];
+        }
+
+        if (sum == 4) {
+            break;
+        }
+    }
+
+    printf("Part One: %d\n", i + 1);
+}
