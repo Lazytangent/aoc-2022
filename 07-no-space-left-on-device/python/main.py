24a7f3eca212a01b0754dbbbeb3bc46d8dac165d
Lazytangent <peter.tn.mai@gmail.com>
2023-04-22 16:45:53 -0500

    Formatting
    lol

--- 07-no-space-left-on-device/python/main.py
+++ 07-no-space-left-on-device/python/main.py
@@ -12,12 +12,12 @@ def parse():
     dir_sizes = {}
 
     for line in contents:
-        line = line.split(' ')
+        line = line.split(" ")
 
         if line[0] == "$":
-            if line[1] == "cd" and line[2] != '..':
+            if line[1] == "cd" and line[2] != "..":
                 parent_dirs.append(line[2])
-                dir_sizes['/'.join(parent_dirs)] = 0
+                dir_sizes["/".join(parent_dirs)] = 0
             elif line[1] == "cd" and line[2] == "..":
                 parent_dirs.pop()
         elif line[0].isnumeric():
@@ -30,21 +30,21 @@ def parse():
     required_free_space = 30_000_000
 
     # print(f"Size of all directories: {dir_sizes}")
-    small_dirs = { k: v for (k, v) in dir_sizes.items() if v <= 100_000 }
+    small_dirs = {k: v for (k, v) in dir_sizes.items() if v <= 100_000}
     print(f"Part one solution: {sum(small_dirs.values())}")
 
-    current_free_space = total_disk_space - dir_sizes['/']
+    current_free_space = total_disk_space - dir_sizes["/"]
     # print(f"Current free space: {current_free_space}")
 
     required_change = required_free_space - current_free_space
     # print(f"Required change: {required_change}")
 
-    eligible_dirs = { k: v for (k, v) in dir_sizes.items() if v >= required_change }
+    eligible_dirs = {k: v for (k, v) in dir_sizes.items() if v >= required_change}
     # print(f"Eligible dirs: {eligible_dirs}")
 
     smallest_eligible_dir = "/"
 
-    for (dir, size) in eligible_dirs.items():
+    for dir, size in eligible_dirs.items():
         if size < eligible_dirs[smallest_eligible_dir]:
             smallest_eligible_dir = dir
 
